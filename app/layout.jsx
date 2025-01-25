e0ef016ce894f62396a3607b83660ab2f80a18ba
Daniël van Ginneken <dcj.vanginneken@student.avans.nl>
2025-01-25 22:14:49 +0100

    Add contact, resume, services, and work pages with transitions

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,10 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+
+// Components
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -18,7 +22,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${jetbrainsMono.variable} antialiased`}>
         <Header />
-        {children}
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
