326be6c438fcbfdc416bf5768c09d9c9b8329af3
Daniel van Ginneken <dcjvanginneken@gmail.com>
2026-04-10 17:00:49 +0200

    Upgrade project to Next.js 16.2.3

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 
 import { defaultLocale, isLocale } from "@/lib/site";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const [, maybeLocale] = pathname.split("/");
 
